ca4883a64ec59070d21a00c6813f43870b3fde5d
Atallah Haddad <haddad1421990@gmail.com>
2024-08-28 20:00:07 -0500

    adjusted copyright font size with inspiredBy

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,16 +3,11 @@ import styles from './FooterStyles.module.css'
 function Footer() {
   return (
     <section id="footer" className={styles.container}>
-      <p>
-        {" "}
+      <p className={styles.copyright}>
         &copy; 2024 Atallah Haddad. <br />
         All rights reserved.
       </p>
-      <br />
-      <br />
-      <br />
-
-      <p>
+      <p className={styles.inspiredBy}>
         Inspired by{" "}
         <a href="https://www.youtube.com/@howtobecomeadeveloper">
           How to Become a Developer
